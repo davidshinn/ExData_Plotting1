676db0559cd2c762317706ff534931bc86577d01
David Shinn <david.shinn@gmail.com>
2014-07-08 22:33:32 -0400

    Fix save to png code

--- plot1.R
+++ plot1.R
@@ -5,11 +5,15 @@ dt <- read.table(DATAFILE_ABRIDGED, header=TRUE, sep=";",
 
 dt$datetime <- strptime(paste(dt$Date, dt$Time),
                         '%d/%m/%Y %H:%M:%S')
-
+message('--- Display Histogram of Global Active Power')
 hist(dt$Global_active_power,
      main='Global Active Power',
      xlab=c('Global Active Power (kilowatts)'),
      col='red')
 message('--- Saving to file plot1.png')
-dev.copy(png, filename='plot1.png')
+png(filename="plot1.png")
+hist(dt$Global_active_power,
+     main='Global Active Power',
+     xlab=c('Global Active Power (kilowatts)'),
+     col='red')
 dev.off()
